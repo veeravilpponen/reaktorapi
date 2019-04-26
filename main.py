b931b78a2ea50addbd3dc9280f5fffeb84627c5d
Veera Vilpponen <vilpponen.veera@myy.haaga-helia.fi>
2019-04-26 11:24:38 +0300

    See population/co2 of selected country and year

--- main.py
+++ main.py
@@ -2,14 +2,35 @@ from flask import Flask
 import csv
 
 selected_country = input("Give a country: ")
+selected_year = input("Give a year (1960-2018): ")
+file_type = input("Select file type (co2/population): ")
 
-with open('../excelit/vakiluvut/vakiluku_tiedosto.csv', 'r') as csvfile:
-    lines = csv.reader(csvfile, delimiter=',')
-    for row in lines:
-        if lines.line_num > 4:
+co2 = '../excelit/paastot/paasto_tiedosto.csv'
+population = '../excelit/vakiluvut/vakiluku_tiedosto.csv'
+
+if file_type == 'co2':
+    file_to_open = co2
+elif file_type == 'population':
+    file_to_open = population
+
+with open(file_to_open, 'r') as csvfile:
+    file = csv.reader(csvfile, delimiter=',')
+
+    # loop through the rows
+    for row in file:
+        # fifth line is a header line
+        if file.line_num is 5:
+            for x in row:
+                # getting index of the selected_year
+                if x == selected_year:
+                    year_index = row.index(x)
+        # countries start after fifth line
+        elif file.line_num > 5:
             country = row[0]
             if country == selected_country:
-                print(row)
+                # co2 / population of selected_country and selected_year
+                print(row[year_index])
+
 
 #app = Flask(__name__)
 #
